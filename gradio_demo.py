201f9b4e84d79bd602006568bb72c3ea4df9c89a
singularity <12184989+singularity-s0@users.noreply.github.com>
2025-09-30 19:18:03 +0800

    fix: missing comma

--- gradio_demo.py
+++ gradio_demo.py
@@ -24,7 +24,7 @@ def parse_args():
     parser.add_argument(
         '--codec_path',
         type=str,
-        default='fnlp/MOSS-Speech-Codec'
+        default='fnlp/MOSS-Speech-Codec',
         help="the path of codec",
     )
     parser.add_argument("--host", type=str, default="0.0.0.0", help="server address")
